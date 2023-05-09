031e68078f1dc1e108b10c442eefe047c31fed88
TLeeming15 <tyler.leeming21@gmail.com>
2023-05-09 13:28:13 +0930

    card work

--- src/Projects.js
+++ src/Projects.js
@@ -8,34 +8,34 @@ return (
         <h2>Projects</h2>
         <main id="projects-cards">
 
-            <article className="card first">
+            <article className="card">
                 <h3>Trivia Application</h3>
-                <img alt="project" src="assets/images/5minute-triva.png"/>
+                <img alt="project" src="https://tleeming15.github.io/my-portfolio/assets/images/trivia-app.png"/>
             </article>
 
             <article className="card">
                 <h3>Skills Match Application</h3>
-                <img alt="project" src="assets/images/skills-match.png"/>
+                <img alt="project" src="https://tleeming15.github.io/my-portfolio/assets/images/homepage.png"/>
             </article>
 
             <article className="card">
-                <h3>Regex Gist</h3>
-                <img alt="project" src="assets/images/regex-gist.png"/>
+                <h3>Tech-Blog</h3>
+                <img alt="project" src="https://github.com/TLeeming15/tech-blog/raw/main/assets/images/login.png"/>
             </article>
 
             <article className="card">
                 <h3>README Generator</h3>
-                <img alt="project" src="assets/images/readme-generator.png"/>
+                <img alt="project" src="https://github.com/TLeeming15/readme-generator/raw/main/Develop/images/example-readme.png"/>
             </article>
 
             <article className="card">
                 <h3>Team Profile Generator</h3>
-                <img alt="project" src="assets/images/team-profile.png"/>
+                <img alt="project" src="https://github.com/TLeeming15/team-profile-generator/raw/main/images/team-generated.png"/>
             </article>
 
             <article className="card">
                 <h3>Password Generator</h3>
-                <img alt="project" src="assets/images/password-generator.png"/>
+                <img alt="project" src="https://github.com/TLeeming15/password-generator/raw/main/assets/images/p-g-generated-password.png"/>
             </article>
            
         </main>
